01b4022989fd0287c40498baf11ed50e38ac8d20
ChoButton <chosy0716@gmail.com>
2023-09-18 22:07:13 +0900

    조승연 : UserUpdate기능 구현, CORS오류로 인해 BasicSecurityConfig내에 기능추가, WebConfig 클래스 추가구현

--- front/src/common/ApiEndpoints.js
+++ front/src/common/ApiEndpoints.js
@@ -3,5 +3,6 @@ const API_BASE = 'http://localhost:8080';
 export const ENDPOINTS = {
     LOGIN: `${API_BASE}/login`,
     SIGNUP: `${API_BASE}/signup`,
+    USER_UPDATE: `${API_BASE}/update/password`,
     CREATE_SHORTEN_URL: `${API_BASE}/url/user/create`
 };
